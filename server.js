d7f93b1cac062b36f1aa0190e5ebfbe39709a4cc
languo <lan.guo14@gmail.com>
2018-01-15 20:36:00 -0800

    added yaml config and load config when server start up

--- server.js
+++ server.js
@@ -1,9 +1,52 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var yaml = require('js-yaml')
 var port = 8080
 
-//create a server object:
+
+
+function loadYAMLConfig() {
+  var config = null;
+  try {
+    config = yaml.safeLoad(fs.readFileSync('./locations.yml', 'utf8'));
+    console.log('YAML config file loaded, showing config.locations:', config.locations);
+} catch (e) {
+    console.log(e);
+}
+  return config;
+}
+
+
+function inGeofence(userLat, userLon, targetLat, targetLon) {
+  var latDelta = userLat - targetLat;
+  var lonDelta = userLon - targetLon;
+  var distanceSquared = latDelta * latDelta + lonDelta * lonDelta;
+  var thresholdDistanceSquared = 0.000000000123; // distance between TBL and Voodoo Doughnuts
+  return distanceSquared <= thresholdDistanceSquared;
+}
+
+
+function huntForTreasure(config, userLat, userLon) {
+  var message = 'Your location has no hidden treasure';
+  var treasure = null;
+  var locations = config.locations;
+  for (var i=0; i<locations.length; i++) {
+    let {address, name, targetLat, targetLon, treasureThisLoc} = locations[i];
+    var nearTreasure = inGeofence(userLat, userLon, targetLat, targetLon);
+    if (nearTreasure) {
+      message = `You are at ${name}`;
+      treasure = treasureThisLoc;
+    }
+  }
+  return {message, treasure};
+}
+
+
+// Load config file when start up server
+var config = loadYAMLConfig();
+
+// create a server object:
 http.createServer(function (req, res) {
   if (req.url === '/index') {
     fs.readFile('index.html', function(err, data) {
@@ -20,9 +63,12 @@ http.createServer(function (req, res) {
   }
   else if (req.url.indexOf('?') === 1) {
     var q = url.parse(req.url, true).query;
-    var coor = 'latitude: ' + q.latitude + '; longitude: ' + q.longitude;
+    //var coor = 'latitude: ' + q.latitude + '; longitude: ' + q.longitude;
+    var latitude = q.latitude;
+    var longitude = q.longitude;
+    var {message, treasure} = huntForTreasure(config, latitude, longitude);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(`The hidden treasure at ${coor} is at ... (some secret gist url)`);
+    res.write(`${message}. The hidden treasure here is ${treasure}.`);
     res.end()
   }
   else {
